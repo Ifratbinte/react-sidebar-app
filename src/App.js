630aafa88f838e91b99ca0a99d94f45a54126d3a
Ifrat Binte <ifratbinte@gmail.com>
2022-11-08 09:56:03 +0600

    remove conflicts

--- src/App.js
+++ src/App.js
@@ -8,15 +8,8 @@ function App() {
   const ctx = useContext(ThemeContext);
   console.log(ctx);
   return (
-    <div className="main">
-      <Sidebar />
-      <div className="main-wrapper">
-        <h1 className="title">My React App</h1>
-        <p className="info">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
-        </p>
-        <button className="btn">Explore now</button>
-      </div>
+    <div className="main" data-theme={ctx?.theme}>
+      <Layout />
     </div>
   );
 }
